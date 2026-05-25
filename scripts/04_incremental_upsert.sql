72625383c460c613118ff15503e28995c0535cb1
parempool <parempool@hotmail.com>
2026-05-25 21:53:17 +0300

    Update 04_incremental_upsert.sql
    Täiendasin skripti delete osas, sest on teada, et andmed võivad varasemates perioodides muutuda

--- scripts/04_incremental_upsert.sql
+++ scripts/04_incremental_upsert.sql
@@ -1,3 +1,15 @@
+/* UNIQUE constraint / index ON CONFLICT jaoks, sest oodatud tulemuses on nende 5 veeru kombinatsioon tabelis unikaalne */
+create unique index if not exists uq_fact_respiratory
+on silver.fact_respiratory_surveillance (
+    yearweek,
+    countryname,
+    survtype,
+    pathogen,
+    indicator
+);
+
+/* UPSERT (INSERT + UPDATE) */
+
 insert into silver.fact_respiratory_surveillance (
     yearweek,
     countryname,
@@ -6,6 +18,7 @@ insert into silver.fact_respiratory_surveillance (
     indicator,
     value
 )
+
 select
     yearweek,
     countryname,
@@ -13,15 +26,26 @@ select
     pathogen,
     indicator,
     sum(value) as value
+
 from bronze.raw_ecdc_tests
-where pathogen in ('Influenza', 'RSV', 'SARS-CoV-2')
-    and indicator in ('detections', 'tests')
+where pathogen in (
+    'Influenza',
+    'RSV',
+    'SARS-CoV-2'
+)
+    
+and indicator in (
+    'detections',
+    'tests'
+)
+
 group by
     yearweek,
     countryname,
     survtype,
     pathogen,
     indicator
+
 
 on conflict (
     yearweek,
@@ -34,3 +58,45 @@ on conflict (
 do update set
     value = excluded.value,
     updated_at = current_timestamp;
+
+
+/* DELETE read, mida source'is enam pole, eemaldame ka tabelist */
+
+delete from silver.fact_respiratory_surveillance tgt
+where not exists (
+    select 1
+    from (
+        select
+            yearweek,
+            countryname,
+            survtype,
+            pathogen,
+            indicator
+
+        from bronze.raw_ecdc_tests
+        where pathogen in (
+            'Influenza',
+            'RSV',
+            'SARS-CoV-2'
+        )
+
+        and indicator in (
+            'detections',
+            'tests'
+        )
+
+        group by
+            yearweek,
+            countryname,
+            survtype,
+            pathogen,
+            indicator
+
+    ) src
+
+    where src.yearweek = tgt.yearweek
+      and src.countryname = tgt.countryname
+      and src.survtype = tgt.survtype
+      and src.pathogen = tgt.pathogen
+      and src.indicator = tgt.indicator
+);
